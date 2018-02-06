d48843f1f852c9a5b41b522a10e8bd992be71725
Joaquin Torres Ruiz <DEVISAB@MacBook-Pro-de-Joaquin.local>
2018-02-06 14:45:56 -0500

    Reload

---
+++ platforms/browser/www/js/DEVISABapp_edge.js
@@ -0,0 +1,70 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+
+
+var resources = [
+];
+var symbols = {
+"stage": {
+   version: "1.5.0",
+   minimumCompatibleVersion: "1.5.0",
+   build: "1.5.0.217",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+         dom: [
+         {
+            id:'Rectangle',
+            type:'rect',
+            rect:['0px','0px','550px','400px','auto','auto'],
+            fill:["rgba(217,211,13,1.00)"],
+            stroke:[0,"rgba(0,0,0,1)","none"]
+         }],
+         symbolInstances: [
+
+         ]
+      },
+   states: {
+      "Base State": {
+         "${_Stage}": [
+            ["color", "background-color", 'rgba(255,255,255,1)'],
+            ["style", "width", '550px'],
+            ["style", "height", '400px'],
+            ["style", "overflow", 'hidden']
+         ],
+         "${_Rectangle}": [
+            ["color", "background-color", 'rgba(217,211,13,1.00)']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 0,
+         autoPlay: true,
+         timeline: [
+         ]
+      }
+   }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-4228592");
